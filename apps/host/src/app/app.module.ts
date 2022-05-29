7c49634d3082c276539b1b5a9a1d66d0feefa769
jaybell <jaycooperbell@gmail.com>
2022-05-29 11:32:23 -0700

    init

--- apps/host/src/app/app.module.ts
+++ apps/host/src/app/app.module.ts
@@ -3,11 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule } from '@angular/router';
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        loadChildren: () =>
+          import('remote/Module').then((m) => m.RemoteEntryModule),
+      },
+    ]),
+    ApolloModule,
+  ],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'https://48p1r2roz4.sse.codesandbox.io',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ apps/remote/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'remote-entry',
+  template: `<mfe-secondary-entry-point-demo-nx-welcome></mfe-secondary-entry-point-demo-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
