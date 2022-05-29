7b651af6e5577d514e41e78e4e46d05153cd9446
jaybell <jaycooperbell@gmail.com>
2022-05-29 11:04:22 -0700

    initial commit

---
+++ apps/host/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mfe-secondary-entry-point-demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'host';
+}
